2ec490150678fd8a2244f66bf0a07b1025298900
Wesley Jones <wesjones2004@gmail.com>
2023-03-28 10:40:28 -1000

    Update build

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build ransomware
-g++ main.cpp -o main -lcrypto  -std=c++17 -Wall -O3
+g++ main.cpp -o main -lcrypto -lpthread -std=c++17 -Wall -O3
 
 # Build decryptor
-g++ decryptor.cpp -o decryptor -lcrypto -std=c++17 -Wall -O3
+g++ decryptor.cpp -o decryptor -lpthread -lcrypto -std=c++17 -Wall -O3
